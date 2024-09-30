ec2d32c7cf99460353c2895737b2f0e502cb70f0
CharanByte <charan7812@gmail.com>
2024-09-30 23:28:51 +0530

    initial

--- React/vite-project/src/React  Props/temp.jsx
+++ React/vite-project/src/React  Props/temp.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 
+let sty={
+   color:"red",
+   width:200,
+   
+}
 function Card(Props){
     
-    return <h1>{Props.name}</h1>
+    return <div>
+        <h1 style={sty}>{Props.name}</h1>
+        <img src={Props.src} style={sty} ></img>
+        <p>{Props.tele}</p>
+        <p>{Props.email}</p>
+    </div> 
 }
 
 export default Card;
--- React/vite-project/src/main.jsx
+++ React/vite-project/src/main.jsx
@@ -21,6 +21,14 @@ import Card from "./React  Props/temp"
 
 ReactDom.render(<div>
   <App></App>
-  <Card name="charan"/>
+  <Card name="charan"
+  src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/3d0f6/MainAfter.avif"
+  tele="9267268278" 
+  email="xyz@gmail.com"/>
+
+  <Card name="sachin"
+  src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
+  tele="782787792" 
+  email="abcd@gmail.com"/>
 
 </div>,document.getElementById("root"));
